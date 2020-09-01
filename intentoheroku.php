6aea2c253ec96ec1a44727abfd6c0762e9ba41ad
Al3x1x <431alexis@gmail.com>
2020-08-31 20:43:59 -0500

    segundo archivo

--- intentoheroku.php
+++ intentoheroku.php
@@ -3,14 +3,17 @@
 <head>
 	<title>DuberCarpinteria</title>
 <meta charset="UFT-8"/>
-<link rel="icon" href="C:\Users\HP\Desktop\Lenguajes para la web\images" type="image/png"/>
+<link rel="icon" type="image/png" href="favicon.png"/>
 <meta name="description" content="Instalación, diseño, venta, fabricación y pintada de cocinas, closets, muebles, bibliotecas, puertas, armarios, repisas"/>
 <meta name="keywords" content="cocinas, cocinas integrales, closets, muebles, bibliotecas, armarios, puertas, intalación"/>
 </head>
 
 <body>
+
+
+
 	<h1>Bienvenido</h1>
-	href="C:\Users\HP\Desktop\Lenguajes para la web\images"
+
 	</p>
 </body>
 </html>
