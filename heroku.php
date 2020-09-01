ddd80bdf058ece2fd6a825f84ed2cf2d622f1440
Al3x1x <431alexis@gmail.com>
2020-08-31 20:30:38 -0500

    Intento 1003920 de subirlo

--- heroku.php
+++ heroku.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title> </title>
+	<title>DuberCarpinteria</title>
+<meta charset="UFT-8"/>
+<link rel="icon" href="C:\Users\HP\Desktop\Lenguajes para la web\images" type="image/png"/>
+<meta name="description" content="Instalación, diseño, venta, fabricación y pintada de cocinas, closets, muebles, bibliotecas, puertas, armarios, repisas"/>
+<meta name="keywords" content="cocinas, cocinas integrales, closets, muebles, bibliotecas, armarios, puertas, intalación"/>
 </head>
+
 <body>
-	DuberCarpinteria<
+	<h1>Bienvenido</h1>
+	href="C:\Users\HP\Desktop\Lenguajes para la web\images"
+	</p>
 </body>
 </html>
